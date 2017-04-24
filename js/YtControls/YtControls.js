7733b345b0d44ab6487f997340cd955ffe0d3b09
ajwehunt <adamjwehunt@gmail.com>
2017-04-23 20:16:51 -0700

    smaller text on mobile, disable refresh-gesture on mobile

---
+++ js/YtControls/YtControls.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import { Toolbar, ToolbarGroup } from 'material-ui/Toolbar'
+import IconButton from 'material-ui/IconButton'
+import './YtControls.css'
+
+const YtControls = React.createClass({
+
+  return render (
+    <div>
+      <Toolbar>
+        
+      </Toolbar>
+    </div>
+  )
+})
+export default YtControls
